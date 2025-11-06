6aee2f85f62f0d1e7b48a1a72f931ba056d807b0
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-11-06 16:53:04 +0500

    feat: add getDNSStats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const res = {};
+  domains.forEach(domain => {
+    const splitted = domain.split('.').reverse();
+    let key = '';
+    for (let i = 0; i < splitted.length; i++)  {
+      key += '.' + splitted[i]
+      res[key] = (res[key] || 0) + 1;
+    }
+  })
+  return res;
 }
 
 module.exports = {
