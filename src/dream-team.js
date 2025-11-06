701930f99a304a061dc4f1f8899028d4be997c1e
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-11-06 17:03:33 +0500

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let name = [];
+  members.forEach(member => {
+    if (typeof member === 'string') {
+      const trim = member.trim()
+      if (trim) {
+        name.push(trim.at(0).toUpperCase());
+      }
+    }
+  })
+  return name.length ? name.sort().join('') : false;
 }
 
 module.exports = {
